1bd17aa58452a7c384042419f17c9b6ee09dcfb9
高晨 <gaochen@maxtropy.com>
2020-06-30 16:27:17 +0800

    Add ibm & aliyun document

---
+++ scripts/help.aliyun.com.js
@@ -0,0 +1,10 @@
+javascript: (function() {
+    $(".help-menu-box").remove();
+    $(".help-category-expand").remove();
+    $(".help-detail-page-turner").remove();
+    $(".recommendation-area").remove();
+    $(".ribbon").remove();
+    $(".help-detail-feedback-wrapper").remove();
+    $(".crumbs").remove();
+    $(".aliyun-topbar-container").remove();
+})();
---
+++ scripts/ibm.com.js
@@ -0,0 +1,4 @@
+javascript: (function () {
+    $("#dw-article-toc-container").remove();
+    $(".ibm-columns.dw-article-toc").className = "markdown";
+})();
